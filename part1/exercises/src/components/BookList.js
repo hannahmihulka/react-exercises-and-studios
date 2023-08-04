3a3832979d9684ed3b826df7b1450a3cf4ccf9af
Hannah Mihulka <hannahmihulka@gmail.com>
2023-08-04 14:27:07 -0500

    added book images and alt text

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "books i have read in the past 6 months";
+   let book1 = "https://languagetrainers.com/reviews/book-reviews/uploads/5679-madame%20bovary%20cover.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51v+v2L6aJL.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/51Z6haCcnQL.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="madame bovary" />
+         <img src={book2} alt="the collected works of william blake" />
+         <img src={book3} alt="the alcoholics anonymous big book" />
       </div>      
    );
 }
--- part1/exercises/src/components/MovieList.js
+++ part1/exercises/src/components/MovieList.js
@@ -6,10 +6,12 @@ class MovieList extends React.Component {
       const movies = ["Raging Bull", "Barbie", "Oppenheimer", "Astroid City"];
       return (
       <div>
-         <h3 className = {classes.movieHeading}>Movies I Watched This Month</h3>
+         <h3 className = {classes.movieHeading}> (-: Movies I Watched This Month :-) </h3>
          <ol>
             <li className={classes.movieText}>{movies[0]}</li>
             <li className={classes.movieText}>{movies[1]}</li>
+            <li className={classes.movieText}>{movies[2]}</li>
+            <li className={classes.movieText}>{movies[3]}</li>
          </ol>
       </div>
       );
