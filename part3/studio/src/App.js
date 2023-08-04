16375e12b1ae34cad00808e8ca9b192cf14e449e
Hannah Mihulka <hannahmihulka@gmail.com>
2023-08-04 13:55:19 -0500

    imported recipieIngredients with fixed spelling

--- part3/studio/src/App.js
+++ part3/studio/src/App.js
@@ -4,7 +4,7 @@ import StatusChange from './components/StatusChange';
 import RecipeDisplay from './components/Recipe'; 
 import RecipeDescription from './components/Description';
 import RecipeAuthor from './components/Description'; 
-import RecipeIngredientss from './components/Ingredients'; 
+import RecipeIngredients from './components/Ingredients'; 
 
 function App() {
   return (
